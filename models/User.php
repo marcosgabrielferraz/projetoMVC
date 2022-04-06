32703312624ec83244d97a09c5f4a16334c7978d
marcosgabrielferraz <69218738+marcosgabrielferraz@users.noreply.github.com>
2022-04-06 19:38:36 -0300

    Implement attribute labels on model and display them in form

--- models/User.php
+++ models/User.php
@@ -8,8 +8,6 @@ namespace app\models;
 
 
 use app\core\DbModel;
-use app\core\Model;
-
 /**
  * Class Register
  *
@@ -35,18 +33,17 @@ class User extends DbModel
         return ['firstname', 'lastname', 'email', 'password'];
     }
 
-    public function rules()
+    public function labels(): array
     {
         return [
-            'firstname' => [self::RULE_REQUIRED],
-            'lastname' => [self::RULE_REQUIRED],
-            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
-                self::RULE_UNIQUE, 'class' => self::class
-            ]],
-            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8]],
-            'passwordConfirm' => [[self::RULE_MATCH, 'match' => 'password']],
+            'firstname' => 'First name',
+            'lastname' => 'Last name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'passwordConfirm' => 'Password Confirm'
         ];
     }
+    
     public function save()
     {
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
