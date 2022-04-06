e446e9af817ad0eb7aabfe83aa6ce4c6f056646d
marcosgabrielferraz <69218738+marcosgabrielferraz@users.noreply.github.com>
2022-04-06 18:06:17 -0300

    Create register view

---
+++ views/register.php
@@ -0,0 +1,28 @@
+<h1>Contact page</h1>
+
+<form>
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label>First name</label>
+                <input type="text" class="form-control" name="lastname">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label>Last name</label>
+                <input type="text" class="form-control" name="lastname">
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control" name="email">
+        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
