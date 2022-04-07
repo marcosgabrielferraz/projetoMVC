4ddd593f4725d7da0dbc530379afbbeba8450256
marcosgabrielferraz <69218738+marcosgabrielferraz@users.noreply.github.com>
2022-04-07 14:03:31 -0300

    Implement saving user in session and selecting when request is made

--- core/Application.php
+++ core/Application.php
@@ -16,22 +16,40 @@ class Application
 {
     public static Application $app;
     poblic static string $ROOT_DIR;
+    public string $userClass;
     public Router $router;
     public Request $request;
     public Response $reponse;
     public Controller $controller;
     public Database $db;
     public Session $session;
+    public DbModel $user;
 
     public function __construct($rootDir, $config)
     {
-      self::$ROOT_DIR = $rootDir
-      self::$app = $this;
+        $this->userClass = $config['userClass'];
+        self::$ROOT_DIR = $rootDir
+        self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
         $this->db = new Database($config['db']);
         $this->session = new Session();
+        
+        $userId = Application::$app->session->get('user');
+        if ($userId) {
+            $key = $this->userClass::primaryKey();
+            $this->user = $this->userClass::findOne([$key => $userId]);
+        }
+    }
+
+    public function login(DbModel $user)
+    {
+        $this->user = $user;
+        $primaryKey = $user->primaryKey();
+        $value = $user->{$primaryKey};
+        Application::$app->session->set('user', $value);
+        return true;
     }
 
     public function run()
